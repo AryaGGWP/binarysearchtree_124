385a73633cb8c4c71590d284b330ab61b86fc956
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-07-02 21:13:41 +0700

    membuat aturan if

--- binarysearchtree_124/binarysearchtree_124.cpp
+++ binarysearchtree_124/binarysearchtree_124.cpp
@@ -43,6 +43,12 @@ public:
 		Node* currentNode = NULL;
 		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
 
+		if (parent == NULL) // If the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode; // Mark the nwe node as ROOT
+			return; // Exit
+		}
+
 		
 
 int main()
